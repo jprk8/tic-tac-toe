c38d88a11ae021c156b3beb28fd70e2095912a69
John Park <176120986+jprk8@users.noreply.github.com>
2024-08-15 15:03:21 -0700

    Create logic to determine winner after each turn

--- script.js
+++ script.js
@@ -10,20 +10,70 @@ const Gameboard = (function() {
         }
     }
 
-    const playTurn = (x, y, Player) => {
-        board[x][y] = Player.shape;
-        // need to check winner after playing turn
+    const playTurn = (row, column, Player) => {
+        // return false if a cell is already occupied
+        const mark = Player.shape;
+        if (board[row][column] === ' ') {
+            board[row][column] = mark;
+        } else {
+            return false;
+        }
+
+        // check winner after playing each turn
+        // check rows, columns, diagonals
+
         printBoard();
+        if (checkWin(Player)) {
+            console.log(`${Player.name} is the winner!`);
+        } else {
+            console.log('no winner');
+        }
     };
 
     const getBoard = () => board;
 
     const printBoard = () => console.table(board);
 
+    const checkWin = (Player) => {
+        // check row
+        const mark = Player.shape;
+
+        for (let i = 0; i < 3; i++) {
+            if (mark === board[i][0] && mark === board[i][1] && mark === board[i][2]) {
+                return true;
+            }
+        }
+
+        // check column
+        for (let j = 0; j <3; j++) {
+            if (mark === board[0][j] && mark === board[1][j] && mark === board[2][j]) {
+                return true;
+            }
+        }
+        //check diagonal
+        if (mark === board[0][0] && mark === board[1][1] && mark === board[2][2]
+            || mark === board[2][0] && mark === board[1][1] && mark === board[0][2]) {
+                return true;
+        }
+
+        return false;
+    }
+
+    const reset = () => {
+        for (let i = 0; i < 3; i++) {
+            board[i] = [];
+            for (let j = 0; j < 3; j++) {
+                board[i][j] = ' ';
+            }
+        }
+    }
+
     return {
         getBoard,
         playTurn,
         printBoard,
+        reset,
+        checkWin,
     }
 })();
 
